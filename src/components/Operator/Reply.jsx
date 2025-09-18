dc44a59369b5ffb1132d7b04cd5dfc93695f6e04
jef70987 <japhethmasinde3@gmail.com>
2025-09-19 01:49:35 +0300

    fix: correct react-icons import

--- src/components/Operator/Reply.jsx
+++ src/components/Operator/Reply.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 import { useSlug } from "../Tenants/TenantOperator";
-import { MdReviews } from 'react-icons/md';
 const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
 
 
