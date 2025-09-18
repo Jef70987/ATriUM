af590b7edd0d091f63a336e42cfdcdbaa2a11fcc
jef70987 <japhethmasinde3@gmail.com>
2025-09-19 02:00:10 +0300

    configure falkon-parlor base path

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import FalkoNParlour from './components/Landing/LandingPage';
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename='/falkon-parlor'>
         
         <Routes>
           <Route path="/falkon-parlor/" element={<FalkoNParlour />} />
