2d85222eca00da06924dc3656a1356b9f7d22371
jef70987 <japhethmasinde3@gmail.com>
2025-09-21 02:07:03 +0300

    latest changes

--- src/components/Shop/LoadingSpinner.jsx
+++ src/components/Shop/LoadingSpinner.jsx
@@ -3,6 +3,7 @@ const LoadingSpinner = ({ small }) => {
     return (
         <div className="loading--container">
             <div className= {small ? "loading--spinner" : "loading--smallSpinner"}></div>
+            <p>Please wait...</p>
         </div>
     );
 };
