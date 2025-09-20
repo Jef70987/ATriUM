a270a65e367fa5e6414a4d5d47bf4ad6176767ac
jef70987 <japhethmasinde3@gmail.com>
2025-09-21 01:58:26 +0300

    latest change

--- src/components/Shop/LoadingSpinner.jsx
+++ src/components/Shop/LoadingSpinner.jsx
@@ -1,33 +1,8 @@
-const styles = {
-    container: {
-        width:'1200px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100px',
-    },
-    spinner: {
-        width: '40px',
-        height: '40px',
-        border: '4px solid #f3f3f3',
-        borderTop: '4px solid #ff6b6b',
-        borderRadius: '50%',
-        animation: 'spin 5s linear infinite',
-    },
-    smallSpinner: {
-        width: '20px',
-        height: '20px',
-        border: '3px solid #f3f3f3',
-        borderTop: '3px solid #ff6b6b',
-        borderRadius: '50%',
-        animation: 'spin 5s linear infinite',
-    },
-};
-
+import './LoadingSpinner.css'
 const LoadingSpinner = ({ small }) => {
     return (
-        <div style={styles.container}>
-        <div style={small ? styles.smallSpinner : styles.spinner}></div>
+        <div className="loading--container">
+            <div className= {small ? "loading--spinner" : "loading--smallSpinner"}></div>
         </div>
     );
 };
