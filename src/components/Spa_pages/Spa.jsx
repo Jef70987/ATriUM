3b654391e7694dfab579c189621543b7e6b9b952
jef70987 <japhethmasinde3@gmail.com>
2025-11-26 20:54:47 +0300

    latest changes

--- src/components/Spa_pages/Spa.jsx
+++ src/components/Spa_pages/Spa.jsx
@@ -14,7 +14,7 @@ import ClientView from "./ClientBookingView";
 const Spa = () => {
     return(
         <div className="flex flex-col min-h-screen">
-        
+            
             <div className="main-display flex-1 w-full">
                 <Routes>
                     <Route path="/" element={
